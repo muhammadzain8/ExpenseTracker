f744c03e48659195317e28e99be716a8f0e85a4b
zain <muhammadzain8@gmail.com>
2021-09-04 12:39:13 +0500

    few changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Details from './components/Details/Details';
+import { Grid } from '@material-ui/core';
+import useStyles from './styles'
 
-function App() {
+
+const App = () => {
+  const classes=useStyles( )
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Grid
+        container
+        spacing={0}
+        alignItems='center'
+        justify='center'
+        className={classes.grid}
+        style={{ height: '100vh' }}
+      >
+        <Grid item xs={12} sm={4}>
+          <Details title='income'/>
+        </Grid>
+        <Grid item xs={12} sm={4}>
+          <main />
+        </Grid>
+        <Grid item xs={12} sm={4}>
+          <Details title='expense'/>
+        </Grid>
+      </Grid>
     </div>
   );
-}
+};
 
 export default App;
